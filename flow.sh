2fefc3d3bb38a1256ea944c54620eb14b3f5be16
Jana Petrovic <petrovic.jana12@gmail.com>
2023-12-19 11:04:41 +0100

    Added flow.sh

---
+++ flow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+exit 0
+
+git pull
+# Do something.
+git status
+git add .
+git status
+git config user.name "Jana Petrovic"
+git config user.email "petrovic.jana12@gmail.com"
+git commit -m "Added lab 7 and joy_node.cpp as test_joypad.cpp to applications."
+git push
+
